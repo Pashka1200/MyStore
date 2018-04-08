837e667d9416540f273cd4b9aaad0fe24d3fff5a
pavlo <cherapaha@gmail.com>
2018-04-08 19:20:45 +0300

    update for: User app properties file

--- src/main/java/ua/petproject/mystore/model/User.java
+++ src/main/java/ua/petproject/mystore/model/User.java
@@ -19,7 +19,6 @@ public class User {
     @Column(name = "password")
     @Length(min = 5, message = "*Your password must have at least 5 characters")
     @NotEmpty(message = "*Please provide your password")
-    @Transient
     private String password;
 
     @Column(name = "email")
